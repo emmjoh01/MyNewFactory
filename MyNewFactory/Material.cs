bf07920ebd551df3e649fe735ac58ce40455cb8a
Emma Johansson <emmajohansson266@gmail.com>
2021-10-14 15:31:39 +0200

    Added Enum with Materials Added Class Storage Added Mathod ShowStorage in Class Storage

---
+++ MyNewFactory/Material.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyNewFactory
+{
+    public enum Material
+    {
+        Fabric = 0,
+        RedPaint = 1,
+        Rubber = 2,
+        Screws = 3,
+        Steel = 4, 
+        Wood = 5,
+    }
+}
---
+++ MyNewFactory/Storage.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyNewFactory
+{
+    class Storage
+    {
+        private List<Material> materialsInStorage = new();
+        int[] materialAmountInStorage = new int[6];
+
+
+
+        public Storage() //Filling storage with materials, random amount between 0 to 15 of each
+        {
+            for (int i = 0; i < 6; i++)
+            {
+                materialsInStorage.Add((Material)i);
+                materialAmountInStorage[i] = new Random().Next(0, 16);
+                //Console.WriteLine($"{i+1}. {materialsInStorage[i], -15} Amount: {materialAmountInStorage[i]}");
+            }
+        }
+
+        public void ShowStorage()
+        {
+            Console.WriteLine("-----Materials in storage-----");
+            for (int i = 0; i < materialsInStorage.Count; i++)
+            {
+                Console.WriteLine($"{i+1}. {materialsInStorage[i], -15} Amount: {materialAmountInStorage[i]}");
+            }
+        }
+
+    }
+}
