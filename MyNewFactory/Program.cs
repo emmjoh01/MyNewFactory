d32baa220a1510174f09f1b47480b05bd0aca5b7
Emma Johansson <emmajohansson266@gmail.com>
2021-10-14 15:06:20 +0200

    Original plan in something simular to Psuedo-code

--- MyNewFactory/Program.cs
+++ MyNewFactory/Program.cs
@@ -6,7 +6,18 @@ namespace MyNewFactory
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Welcome-message
+            //While 
+                //While
+                    //Show materials in storage
+                    //User picks materials
+                    //Shows earlier picked material
+
+                //Material sent to factory
+                //Chosen materialn matches with products
+                //Returning pruducts and material-leftovers
+                //Check if anything is possible to make with what's left in storage
+            //Message material out of stock or nothing more to make. Game's ended
         }
     }
 }
